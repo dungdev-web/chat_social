8a603f710279d36550cfdbaf2351d0a7456d2349
LƯU ĐỨC DŨNG <dung.dev.web@gmail.com>
2026-02-03 21:22:13 +0700

    up

--- chat-realtime/src/lib/socket.ts
+++ chat-realtime/src/lib/socket.ts
@@ -1,3 +1,7 @@
 import { io } from "socket.io-client";
-
-export const socket = io("http://localhost:3001");
+export const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  transports: ["websocket"],
+  reconnection: true,
+  reconnectionAttempts: 5,
+  timeout: 20000
+});

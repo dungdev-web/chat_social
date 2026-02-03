10a7d48997690ce5d0ddbad3a2de2f2bff3ad370
LƯU ĐỨC DŨNG <dung.dev.web@gmail.com>
2026-02-03 21:29:28 +0700

    up

--- chat-realtime/src/lib/socket.ts
+++ chat-realtime/src/lib/socket.ts
@@ -1,4 +1,6 @@
 import { io } from "socket.io-client";
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+
 export const socket = io(import.meta.env.VITE_SOCKET_URL, {
   transports: ["websocket"],
   reconnection: true,
